c9583c1c9a0921f8b5e70c3766324976317c16c2
Jorge Elias <eliasmaxil@hotmail.com>
2023-03-06 12:23:11 +0100

    first commit

---
+++ decorators/decorators.py
@@ -0,0 +1,53 @@
+# Functions used in the tutorial of decorators
+
+import functools # For introspection
+import time
+
+def do_twice(func):
+    @functools.wraps(func) # For introspection
+    def wrapper_do_twice(*args, **kwargs):
+        # 1st execution of the input function
+        func(*args, **kwargs)
+        # 2nd execution & return of the output generated in func()
+        return func(*args, **kwargs)  
+    # With this, do_twice will return the wrapper function
+    return wrapper_do_twice
+
+# Pass an argument to a decorator (extension to do_twice)
+def repeat(num_times):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper_repeat(*args, **kwargs):
+            for _ in range(num_times):
+                value = func(*args, **kwargs)
+            return value
+        return wrapper_repeat
+    return decorator_repeat
+
+def timer(func):
+    """Print the runtime of the decorated function"""
+    @functools.wraps(func)
+    def wrapper_timer(*args, **kwargs):
+        start_time = time.perf_counter()    # 1
+        value = func(*args, **kwargs)
+        end_time = time.perf_counter()      # 2
+        run_time = end_time - start_time    # 3
+        print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
+        return value
+    return wrapper_timer
+
+
+def debug(func):
+    """Print the function signature and return value"""
+    @functools.wraps(func)
+    def wrapper_debug(*args, **kwargs):
+        args_repr = [repr(a) for a in args]                      # 1
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
+        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        print(f"Calling {func.__name__}({signature})")
+        value = func(*args, **kwargs)
+        print(f"{func.__name__!r} returned {value!r}")           # 4
+        return value
+    return wrapper_debug
+
+
